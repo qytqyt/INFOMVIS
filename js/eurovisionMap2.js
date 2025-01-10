92d73467d9188407505ca8916649ec365e6de0d3
qytqyt <68152955+qytqyt@users.noreply.github.com>
2025-01-10 02:04:15 +0100

    Add files via upload

---
+++ js/eurovisionMap2.js
@@ -0,0 +1,250 @@
+// List of Eurovision participating countries
+const eurovisionCountries = [
+    'Albania', 'Armenia', 'Australia', 'Austria', 'Azerbaijan',
+    'Belgium', 'Bosnia and Herzegovina', 'Bulgaria', 'Croatia', 'Cyprus',
+    'Czech Republic', 'Denmark', 'Estonia', 'Finland', 'France',
+    'Georgia', 'Germany', 'Greece', 'Hungary', 'Iceland',
+    'Ireland', 'Israel', 'Italy', 'Latvia', 'Lithuania',
+    'Malta', 'Moldova', 'Monaco', 'Montenegro', 'Netherlands',
+    'North Macedonia', 'Norway', 'Poland', 'Portugal', 'Romania',
+    'Russia', 'San Marino', 'Serbia', 'Serbia and Montenegro', 'Slovakia',
+    'Slovenia', 'Spain', 'Sweden', 'Switzerland', 'Turkey',
+    'Ukraine', 'England'
+];
+const countryCodeToName = {
+    'ad': 'Andorra',
+    'al': 'Albania',
+    'am': 'Armenia',
+    'at': 'Austria',
+    'au': 'Australia',
+    'az': 'Azerbaijan',
+    'ba': 'Bosnia and Herzegovina',
+    'be': 'Belgium',
+    'bg': 'Bulgaria',
+    'by': 'Belarus',
+    'ch': 'Switzerland',
+    'cy': 'Cyprus',
+    'cz': 'Czech Republic',
+    'de': 'Germany',
+    'dk': 'Denmark',
+    'ee': 'Estonia',
+    'es': 'Spain',
+    'fi': 'Finland',
+    'fr': 'France',
+    'gb': 'England',
+    'ge': 'Georgia',
+    'gr': 'Greece',
+    'hr': 'Croatia',
+    'hu': 'Hungary',
+    'ie': 'Ireland',
+    'il': 'Israel',
+    'is': 'Iceland',
+    'it': 'Italy',
+    'lt': 'Lithuania',
+    'lu': 'Luxembourg',
+    'lv': 'Latvia',
+    'ma': 'Morocco',
+    'mc': 'Monaco',
+    'md': 'Moldova',
+    'me': 'Montenegro',
+    'mk': 'North Macedonia',
+    'mt': 'Malta',
+    'nl': 'Netherlands',
+    'no': 'Norway',
+    'pl': 'Poland',
+    'pt': 'Portugal',
+    'ro': 'Romania',
+    'rs': 'Serbia',
+    'ru': 'Russia',
+    'se': 'Sweden',
+    'si': 'Slovenia',
+    'sk': 'Slovakia',
+    'sm': 'San Marino',
+    'tr': 'Turkey',
+    'ua': 'Ukraine',
+};
+
+function initializeMap(initialVoteData) {
+    filteredVoteData = initialVoteData;
+
+    const width = 960;
+    const height = 600;
+
+    const svg = d3.select('#map')
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height);
+
+    const tooltip = d3.select('body')
+        .append('div')
+        .attr('class', 'tooltip')
+        .style('opacity', 0);
+
+    const projection = d3.geoMercator()
+        .scale(650)
+        .center([20, 52])
+        .translate([width / 2, height / 2]);
+
+    const path = d3.geoPath().projection(projection);
+
+    const zoom = d3.zoom()
+        .scaleExtent([1, 8])
+        .on('zoom', function(event) {
+            svg.selectAll('path')
+                .attr('transform', event.transform);
+        });
+
+    svg.call(zoom);
+
+    d3.json('https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson')
+        .then(function(geoData) {
+            const eurovisionData = geoData.features.filter(d =>
+                eurovisionCountries.includes(d.properties.name)
+            );
+
+            svg.selectAll('path')
+                .data(eurovisionData)
+                .enter()
+                .append('path')
+                .attr('d', path)
+                .attr('class', 'country')
+                .style('fill', '#69b3a2')
+                .on('mouseover', handleMouseOver)
+                .on('mousemove', handleMouseMove)
+                .on('mouseout', handleMouseOut)
+                .on('click', handleCountryClick);
+        });
+}
+
+function handleMouseOver(event, d) {
+    const hoverData = d3.select('body').node()._hoverData || {};
+    const { clickedCountry, voters } = hoverData;
+
+    let votesInfo = '';
+    const votesByCountry = calculateVotesByCountry(filteredVoteData);
+
+    if (clickedCountry && votesByCountry[d.properties.name]) {
+        const totalVotes = votesByCountry[d.properties.name][clickedCountry] || 0;
+        if (totalVotes > 0) {
+            votesInfo = `<br><strong>Votes Given:</strong> ${totalVotes}`;
+        }
+    }
+
+    if (!d3.select(this).classed('clicked')) {
+        d3.select(this).style('fill', '#ff6347');
+    }
+
+    d3.select('.tooltip')
+        .style('opacity', 1)
+        .html(`<strong>Country:</strong> ${d.properties.name}${votesInfo}`)
+        .style('left', (event.pageX + 10) + 'px')
+        .style('top', (event.pageY - 20) + 'px');
+}
+
+function handleMouseMove(event) {
+    d3.select('.tooltip')
+        .style('left', (event.pageX + 10) + 'px')
+        .style('top', (event.pageY - 20) + 'px');
+}
+
+function handleMouseOut() {
+    if (!d3.select(this).classed('clicked')) {
+        d3.select(this).style('fill', '#69b3a2');
+    }
+    d3.select('.tooltip').style('opacity', 0);
+}
+
+function handleCountryClick(event, d) {
+    const clickedCountry = d.properties.name;
+    const votesByCountry = calculateVotesByCountry(filteredVoteData);
+
+    const voters = [];
+    let maxPoints = 0;
+    let minPoints = Infinity;
+
+    if (votesByCountry[clickedCountry]) {
+        Object.entries(votesByCountry).forEach(([fromCountry, toCountryVotes]) => {
+            const totalPoints = toCountryVotes[clickedCountry] || 0;
+            if (totalPoints > 100) {
+                voters.push({ country: fromCountry, points: totalPoints });
+                maxPoints = Math.max(maxPoints, totalPoints);
+                minPoints = Math.min(minPoints, totalPoints);
+                console.log(`Filtered data - ${fromCountry} gave ${totalPoints} points to ${clickedCountry}`);
+            }
+        });
+    }
+
+    d3.select('body').node()._hoverData = {
+        clickedCountry,
+        voters,
+        minPoints,
+        maxPoints
+    };
+
+    const svg = d3.select('#map svg');
+    svg.selectAll('.country')
+        .style('fill', '#69b3a2')
+        .classed('clicked', false);
+
+    d3.select(event.currentTarget)
+        .style('fill', 'rgb(0, 0, 0)')
+        .classed('clicked', true);
+
+    svg.selectAll('.country')
+        .filter(function(d) {
+            const voter = voters.find(v => v.country === d.properties.name);
+            if (voter) {
+                const intensity = calculateColorIntensity(voter.points, minPoints, maxPoints);
+                const redValue = Math.round(255 - (intensity / 100) * 255);
+                const greenValue = Math.round((intensity / 100) * 50);
+                const colorValue = `rgb(${255 - redValue}, ${greenValue}, ${greenValue})`;
+
+                d3.select(this).style('fill', colorValue);
+                return true;
+            }
+            return false;
+        })
+        .classed('clicked', true);
+}
+
+function calculateVotesByCountry(voteData) {
+    const votesByCountry = {};
+    voteData.forEach(row => {
+        const toCountry = countryCodeToName[row.to_country_id];
+        const fromCountry = countryCodeToName[row.from_country_id];
+        const totalPoints = parseInt(row.total_points, 10);
+
+        if (toCountry && fromCountry && totalPoints > 0) {
+            if (!votesByCountry[toCountry]) {
+                votesByCountry[toCountry] = {};
+            }
+            if (!votesByCountry[toCountry][fromCountry]) {
+                votesByCountry[toCountry][fromCountry] = 0;
+            }
+            votesByCountry[toCountry][fromCountry] += totalPoints;
+        }
+    });
+    return votesByCountry;
+}
+
+function calculateColorIntensity(points, minPoints, maxPoints) {
+    if (maxPoints === minPoints) {
+        return 75;
+    }
+    return 100 - ((points - minPoints) / (maxPoints - minPoints)) * 75;
+}
+
+function updateMapVisualization(filteredVotes) {
+    filteredVoteData = filteredVotes;
+
+    const svg = d3.select('#map svg');
+    svg.selectAll('.country')
+        .style('fill', '#69b3a2')
+        .classed('clicked', false);
+
+    const clickedElement = svg.select('.clicked').node();
+    if (clickedElement) {
+        const event = new Event('click');
+        clickedElement.dispatchEvent(event);
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,13 +1,280 @@
 let scatterplot;
+let map;
+let selectedCountry = null;
+let startYear = null;
+let endYear = null;
+let filteredVoteData = []; // Add this global variable
 
-loadData();
+function initializeApp() {
+    createFilterControls();
+    loadData();
+}
+
+function createFilterControls() {
+    const filterContainer = d3.select('#filter-container')
+        .append('div')
+        .attr('class', 'filters');
+
+    const yearFilter = filterContainer.append('div')
+        .attr('class', 'filter-group');
+
+    yearFilter.append('label')
+        .text('Start Year: ');
+
+    yearFilter.append('select')
+        .attr('id', 'start-year-select')
+        .attr('class', 'filter-select')
+        .on('change', function() {
+            startYear = this.value || null;
+            if (startYear) {
+                updateEndYearOptions();
+            }
+            updateVisualizations();
+        });
+
+    yearFilter.append('label')
+        .text('End Year: ');
+
+    yearFilter.append('select')
+        .attr('id', 'end-year-select')
+        .attr('class', 'filter-select')
+        .on('change', function() {
+            endYear = this.value || null;
+            if (endYear) {
+                updateStartYearOptions();
+            }
+            updateVisualizations();
+        });
+
+    const countryFilter = filterContainer.append('div')
+        .attr('class', 'filter-group');
+
+    countryFilter.append('label')
+        .text('Country: ');
+
+    countryFilter.append('select')
+        .attr('id', 'country-select')
+        .attr('class', 'filter-select')
+        .on('change', function() {
+            selectedCountry = this.value;
+            updateVisualizations();
+        });
+}
+
 function loadData() {
-    d3.csv("resources/contestants.csv").then((csvData) => {
-        // prepare data
-        let data = csvData;
-
-        scatterplot = new Scatterplot("scatterplot-container", data);
-
-        scatterplot.initVis();
+    Promise.all([
+        d3.csv("resources/contestants.csv"),
+        d3.csv("resources/votes.csv")
+    ]).then(([contestantsData, votesData]) => {
+        processData(contestantsData, votesData);
+        initializeFilters(contestantsData);
+        initializeVisualizations(contestantsData, votesData);
     });
 }
+
+function processData(contestantsData, votesData) {
+    contestantsData.forEach(d => {
+        d.year = +d.year;
+        d.place_contest = +d.place_contest;
+    });
+
+    votesData.forEach(d => {
+        d.year = +d.year;
+        d.total_points = +d.total_points;
+    });
+}
+
+function initializeFilters(contestantsData) {
+    const years = [...new Set(contestantsData.map(d => d.year))].sort();
+
+    // Select start year
+    const startYearSelect = d3.select('#start-year-select');
+    startYearSelect.append('option')
+        .attr('value', '')
+        .text('Select Start Year');
+
+    years.forEach(year => {
+        startYearSelect.append('option')
+            .attr('value', year)
+            .text(year);
+    });
+
+    // Select end year
+    const endYearSelect = d3.select('#end-year-select');
+    endYearSelect.append('option')
+        .attr('value', '')
+        .text('Select End Year');
+
+    years.forEach(year => {
+        endYearSelect.append('option')
+            .attr('value', year)
+            .text(year);
+    });
+
+    // Select country
+    const countries = [...new Set(contestantsData.map(d => d.country))].sort();
+    const countrySelect = d3.select('#country-select');
+    countrySelect.append('option')
+        .attr('value', '')
+        .text('Select Country');
+
+    countries.forEach(country => {
+        countrySelect.append('option')
+            .attr('value', country)
+            .text(country);
+    });
+}
+
+function filterData(data) {
+    let filteredData = data;
+
+    if (startYear || endYear) {
+        const start = startYear ? +startYear : -Infinity;
+        const end = endYear ? +endYear : Infinity;
+
+        filteredData = filteredData.filter(d => {
+            const year = +d.year;
+            return year >= start && year <= end;
+        });
+    }
+
+    if (selectedCountry) {
+        if ('country' in filteredData[0]) {
+            filteredData = filteredData.filter(d => d.country === selectedCountry);
+        } else {
+            filteredData = filteredData.filter(d =>
+                d.from_country_id === selectedCountry ||
+                d.to_country_id === selectedCountry
+            );
+        }
+    }
+
+    return filteredData;
+}
+
+function updateVisualizations() {
+    let filteredContestantData = filterData(scatterplot.data);
+    let newFilteredVoteData = filterData(map.data);
+
+    filteredVoteData = newFilteredVoteData;
+
+    if (scatterplot) {
+        const originalData = scatterplot.data;
+        scatterplot.data = filteredContestantData;
+        scatterplot.displayData = scatterplot.transformData();
+        scatterplot.updateVis();
+        scatterplot.data = originalData;
+    }
+
+    if (map) {
+        updateMapVisualization(newFilteredVoteData);
+    }
+}
+
+function initializeVisualizations(contestantsData, votesData) {
+    scatterplot = new Scatterplot("scatterplot-container", contestantsData);
+    scatterplot.initVis();
+
+    initializeMap(votesData);
+    map = {
+        data: votesData,
+        svg: d3.select('#map svg')
+    };
+}
+
+// Update end year options based on selected start year
+function updateEndYearOptions() {
+    if (!startYear) return;
+
+    const endYearSelect = d3.select('#end-year-select');
+    const currentEndYear = endYearSelect.property('value');
+
+    // Get all available years from both contestants and votes data
+    const contestantYears = [...new Set(scatterplot.data.map(d => d.year))];
+    const voteYears = [...new Set(map.data.map(d => d.year))];
+    const years = [...new Set([...contestantYears, ...voteYears])].sort();
+
+    // Filter years that are greater than or equal to start year
+    const validEndYears = years.filter(year => year >= +startYear);
+
+    // Keep current selection if it's valid
+    const shouldKeepCurrentYear = currentEndYear && +currentEndYear >= +startYear;
+
+    // Store current selection
+    const currentSelection = endYearSelect.property('value');
+
+    // Remove all existing options
+    endYearSelect.selectAll('option').remove();
+
+    // Add default option
+    endYearSelect.append('option')
+        .attr('value', '')
+        .text('Select End Year')
+        .property('selected', !currentSelection || !shouldKeepCurrentYear);
+
+    // Add year options
+    validEndYears.forEach(year => {
+        endYearSelect.append('option')
+            .attr('value', year)
+            .text(year)
+            .property('selected', year.toString() === currentSelection && shouldKeepCurrentYear);
+    });
+
+    // Update end year if current selection is no longer valid
+    if (!shouldKeepCurrentYear) {
+        endYear = null;
+    }
+
+    // Update visualizations
+    updateVisualizations();
+}
+
+// Update start year options based on selected end year
+function updateStartYearOptions() {
+    if (!endYear) return;
+
+    const startYearSelect = d3.select('#start-year-select');
+    const currentStartYear = startYearSelect.property('value');
+
+    // Get all available years from both contestants and votes data
+    const contestantYears = [...new Set(scatterplot.data.map(d => d.year))];
+    const voteYears = [...new Set(map.data.map(d => d.year))];
+    const years = [...new Set([...contestantYears, ...voteYears])].sort();
+
+    // Filter years that are less than or equal to end year
+    const validStartYears = years.filter(year => year <= +endYear);
+
+    // Keep current selection if it's valid
+    const shouldKeepCurrentYear = currentStartYear && +currentStartYear <= +endYear;
+
+    // Store current selection
+    const currentSelection = startYearSelect.property('value');
+
+    // Remove all existing options
+    startYearSelect.selectAll('option').remove();
+
+    // Add default option
+    startYearSelect.append('option')
+        .attr('value', '')
+        .text('Select Start Year')
+        .property('selected', !currentSelection || !shouldKeepCurrentYear);
+
+    // Add year options
+    validStartYears.forEach(year => {
+        startYearSelect.append('option')
+            .attr('value', year)
+            .text(year)
+            .property('selected', year.toString() === currentSelection && shouldKeepCurrentYear);
+    });
+
+    // Update start year if current selection is no longer valid
+    if (!shouldKeepCurrentYear) {
+        startYear = null;
+    }
+
+    // Update visualizations
+    updateVisualizations();
+}
+
+// Initialize app when document is loaded
+document.addEventListener('DOMContentLoaded', initializeApp);
