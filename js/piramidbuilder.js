ec11925eeaabafd2e2b100eadd4a603ba640dc5f
Lucas del Arco <82089509+ArcoLucas@users.noreply.github.com>
2025-01-14 18:35:57 +0100

    Update piramidbuilder.js

--- js/piramidbuilder.js
+++ js/piramidbuilder.js
@@ -1,11 +1,16 @@
 function pyramidBuilder(data, target, options, country) {
+
+  d3.select(target).selectAll("*").remove();
+
   const barHeight = options.barHeight || 20;
   const padding = options.padding || 10;
   const numBars = data.length;
   const totalHeight = numBars * (barHeight + padding);
-  const fixedHeight = options.height || 400;
-  const width = options.width || 500;
 
+  const containerWidth = d3.select(target).node().clientWidth;
+  const containerHeight = d3.select(target).node().clientHeight;
+
+  //  config
   const margin = {
     top: 20,
     right: 0,
@@ -13,9 +18,12 @@ function pyramidBuilder(data, target, options, country) {
     left: 0,
     middle: 20,
   };
-  const sectorWidth = width / 2 - margin.middle;
+
+
+  const sectorWidth = containerWidth / 2 - margin.middle;
   const leftBegin = sectorWidth - margin.left;
-  const rightBegin = width - margin.right - sectorWidth;
+  const rightBegin = containerWidth - margin.right - sectorWidth;
+
 
   const style = {
     leftBarColor: options.style?.leftBarColor || '#006837',
@@ -24,179 +32,218 @@ function pyramidBuilder(data, target, options, country) {
     tooltipColor: options.style?.tooltipColor || '#202020',
   };
 
+  // Max value for x-scales
   const maxValue = Math.ceil(
-    Math.max(
-      d3.max(data, (d) => d.tele_points),
-      d3.max(data, (d) => d.jury_points)
-    )
+      Math.max(
+          d3.max(data, (d) => d.tele_points+20),
+          d3.max(data, (d) => d.jury_points+20)
+      )
   );
 
+  // Scales
   const xScaleLeft = d3.scaleLinear().domain([0, maxValue]).range([sectorWidth, 0]);
   const xScaleRight = d3.scaleLinear().domain([0, maxValue]).range([0, sectorWidth]);
   const yScale = d3
-    .scaleBand()
-    .domain(data.map((d) => d.to_country))
-    .range([0, totalHeight])
-    .padding(0.1);
+      .scaleBand()
+      .domain(data.map((d) => d.to_country))
+      .range([0, totalHeight])
+      .padding(0.1);
 
-  const container = d3
-    .select(target)
-    .style("height", `${fixedHeight}px`)
-    .style("overflow-y", "scroll")
-    .style("border", "1px solid #ccc");
+  // Container
+  const container = d3.select(target)
+      // The container is already sized in CSS.
+      .style("overflow-y", "auto");
 
+  // Create SVG
   const svg = container
-    .append('svg')
-    .attr('width', width)
-    .attr('height', totalHeight + margin.top + margin.bottom);
+      .append('svg')
+      .attr('width', containerWidth)
+      .attr('height', totalHeight + margin.top + margin.bottom);
 
   const chartGroup = svg
-    .append('g')
-    .attr('transform', `translate(${margin.left},${margin.top})`);
+      .append('g')
+      .attr('transform', `translate(${margin.left},${margin.top})`);
 
-  // --- GRUPO PARA BARRAS IZQUIERDAS (TELEVOTO) ---
+  // Left bars (Televote)
   const leftBarGroup = chartGroup
-    .append('g')
-    .attr('transform', `translate(${leftBegin},0) scale(-1,1)`);
+      .append('g')
+      .attr('transform', `translate(${leftBegin},0) scale(-1,1)`);
 
   leftBarGroup
-    .selectAll('.bar.left')
-    .data(data)
-    .enter()
-    .append('rect')
-    .attr('class', 'bar left')
-    .attr('x', 0)
-    .attr('y', (d) => yScale(d.to_country))
-    .attr('width', (d) => xScaleLeft(0) - xScaleLeft(d.tele_points))
-    .attr('height', yScale.bandwidth())
-    // Cambiamos el color si d.to_country === country
-    .attr('fill', (d) => d.to_country === country ? 'black' : style.leftBarColor);
+      .selectAll('.bar.left')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('class', 'bar left')
+      .attr('x', 0)
+      .attr('y', (d) => yScale(d.to_country))
+      .attr('width', (d) => xScaleLeft(0) - xScaleLeft(d.tele_points))
+      .attr('height', yScale.bandwidth())
+      .attr('fill', (d) => d.to_country === country ? 'black' : style.leftBarColor);
 
-  // --- GRUPO PARA BARRAS DERECHAS (JURADO) ---
+  // Right bars (Jury)
   const rightBarGroup = chartGroup
-    .append('g')
-    .attr('transform', `translate(${rightBegin},0)`);
+      .append('g')
+      .attr('transform', `translate(${rightBegin},0)`);
 
   rightBarGroup
-    .selectAll('.bar.right')
-    .data(data)
-    .enter()
-    .append('rect')
-    .attr('class', 'bar right')
-    .attr('x', 0)
-    .attr('y', (d) => yScale(d.to_country))
-    .attr('width', (d) => xScaleRight(d.jury_points))
-    .attr('height', yScale.bandwidth())
-    // Cambiamos el color si d.to_country === country
-    .attr('fill', (d) => d.to_country === country ? 'black' : style.rightBarColor);
+      .selectAll('.bar.right')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('class', 'bar right')
+      .attr('x', 0)
+      .attr('y', (d) => yScale(d.to_country))
+      .attr('width', (d) => xScaleRight(d.jury_points))
+      .attr('height', yScale.bandwidth())
+      .attr('fill', (d) => d.to_country === country ? 'black' : style.rightBarColor);
 
-  // --- TEXTOS EN MEDIO (LABELS) ---
+  // Middle labels
   chartGroup
-    .selectAll('.label.middle')
-    .data(data)
-    .enter()
-    .append('text')
-    .attr('class', 'label middle')
-    .attr('x', width / 2)
-    .attr('y', (d) => yScale(d.to_country) + yScale.bandwidth() / 2)
-    .attr('dy', '0.35em')
-    .attr('text-anchor', 'middle')
-    .text((d) => d.to_country);
+      .selectAll('.label.middle')
+      .data(data)
+      .enter()
+      .append('text')
+      .attr('class', 'label middle')
+      .attr('x', containerWidth / 2)
+      .attr('y', (d) => yScale(d.to_country) + yScale.bandwidth() / 2)
+      .attr('dy', '0.35em')
+      .attr('text-anchor', 'middle')
+      .text((d) => d.to_country);
 
-  // --- EJES ---
+  // Axes
   const yAxisLeft = d3.axisRight(yScale).tickSize(0).tickFormat('');
   const yAxisRight = d3.axisLeft(yScale).tickSize(0).tickFormat('');
   const xAxisLeft = d3.axisBottom(xScaleLeft).ticks(5);
   const xAxisRight = d3.axisBottom(xScaleRight).ticks(5);
 
   chartGroup
-    .append('g')
-    .attr('class', 'axis y left')
-    .attr('transform', `translate(${leftBegin},0)`)
-    .call(yAxisLeft);
+      .append('g')
+      .attr('class', 'axis y left')
+      .attr('transform', `translate(${leftBegin},0)`)
+      .call(yAxisLeft);
 
   chartGroup
-    .append('g')
-    .attr('class', 'axis y right')
-    .attr('transform', `translate(${rightBegin},0)`)
-    .call(yAxisRight);
+      .append('g')
+      .attr('class', 'axis y right')
+      .attr('transform', `translate(${rightBegin},0)`)
+      .call(yAxisRight);
 
   chartGroup
-    .append('g')
-    .attr('class', 'axis x left')
-    .attr('transform', `translate(0,${totalHeight})`)
-    .call(xAxisLeft);
+      .append('g')
+      .attr('class', 'axis x left')
+      .attr('transform', `translate(0,${totalHeight})`)
+      .call(xAxisLeft);
 
   chartGroup
-    .append('g')
-    .attr('class', 'axis x right')
-    .attr('transform', `translate(${rightBegin},${totalHeight})`)
-    .call(xAxisRight);
+      .append('g')
+      .attr('class', 'axis x right')
+      .attr('transform', `translate(${rightBegin},${totalHeight})`)
+      .call(xAxisRight);
 
-  // Label televote axis
+  // Axis Labels
   chartGroup
-    .append("text")
-    .attr("class", "label axis-left")
-    .attr("x", leftBegin / 2)
-    .attr("y", totalHeight + 35)
-    .attr("text-anchor", "middle")
-    .text("Televote");
+      .append("text")
+      .attr("class", "label axis-left")
+      .attr("x", leftBegin / 2)
+      .attr("y", totalHeight + 35)
+      .attr("text-anchor", "middle")
+      .text("Televote");
 
-  // Label jury vote axis
   chartGroup
-    .append("text")
-    .attr("class", "label axis-right")
-    .attr("x", rightBegin + sectorWidth / 2)
-    .attr("y", totalHeight + 35)
-    .attr("text-anchor", "middle")
-    .text("Jury Vote");
+      .append("text")
+      .attr("class", "label axis-right")
+      .attr("x", rightBegin + sectorWidth / 2)
+      .attr("y", totalHeight + 35)
+      .attr("text-anchor", "middle")
+      .text("Jury Vote");
 
-  // --- TOOLTIP ---
+  // Tooltip
   const tooltipDiv = d3.select(target)
-    .append("div")
-    .attr("class", "tooltip")
-    .style("position", "absolute")
-    .style("opacity", 0)
-    .style("pointer-events", "none")
-    .style("background", style.tooltipBG)
-    .style("color", style.tooltipColor)
-    .style("padding", "5px")
-    .style("border", "1px solid #ccc")
-    .style("border-radius", "5px");
+      .append("div")
+      .attr("class", "tooltip");
 
   svg.selectAll("rect")
-    .on("mouseover", function (event, d) {
-      // Resalta las barras con el mismo país en amarillo
-      svg.selectAll("rect")
-        .filter((barData) => barData.to_country === d.to_country)
-        .attr("fill", "yellow");
+      .on("mouseover", function (event, d) {
+        // Highlight bars with same country
+        svg.selectAll("rect")
+            .filter((barData) => barData.to_country === d.to_country)
+            .attr("fill", "yellow");
 
-      // Muestra tooltip
-      tooltipDiv.transition().duration(200).style("opacity", 0.9);
-      tooltipDiv.html(
-        `<strong>${d.to_country}</strong><br/>
-         Tele Points: ${d.tele_points}<br/>
-         Jury Points: ${d.jury_points}`
-      );
-    })
-    .on("mousemove", function (event) {
-      const [x, y] = d3.pointer(event, svg.node());
-      tooltipDiv
-        .style("left", `${x + 1000}px`)
-        .style("top", `${y + 15}px`);
-    })
-    .on("mouseout", function (event, d) {
-      // Vuelve a los colores originales (o negro si coincide con 'country')
-      svg.selectAll("rect")
-        .filter((barData) => barData.to_country === d.to_country)
-        .attr("fill", (barData) =>
-          // Si coincide con el country que pasamos, lo dejamos en negro
-          barData.to_country === country 
-            ? 'black'
-            : (barData.tele_points === d.tele_points ? style.leftBarColor : style.rightBarColor)
+        tooltipDiv.transition().duration(200).style("opacity", 0.9);
+        tooltipDiv.html(
+            `<strong>${d.to_country}</strong><br/>
+           Tele Points: ${d.tele_points}<br/>
+           Jury Points: ${d.jury_points}`
         );
+      })
+      .on("mousemove", function (event) {
+        const [x, y] = d3.pointer(event, svg.node());
+        tooltipDiv
+            .style("left", (x + 20) + "px")
+            .style("top", (y + 20) + "px");
+      })
+      .on("mouseout", function (event, d) {
+        svg.selectAll("rect")
+            .filter((barData) => barData.to_country === d.to_country)
+            .attr("fill", (barData) =>
+                barData.to_country === country
+                    ? 'black'
+                    : (barData.tele_points === d.tele_points
+                        ? style.leftBarColor
+                        : style.rightBarColor)
+            );
 
-      // Oculta tooltip
-      tooltipDiv.transition().duration(500).style("opacity", 0);
-    });
+        tooltipDiv.transition().duration(500).style("opacity", 0);
+      });
 }
+
+
+function toggleFullscreen(target, data, options, country) {
+  const container = d3.select(target);
+  const isFullscreen = container.classed("fullscreen");
+  container.classed("fullscreen", !isFullscreen);
+
+  
+  pyramidBuilder(data, target, options, country);
+
+  d3.select(target)
+      .append("div")
+      .attr("class", "expand-icon")
+      .text("⤢")
+      .on("click", () => toggleFullscreen(target, data, options, country));
+}
+
+
+d3.csv('resources/sampledatapiramid.csv').then((data) => {
+  data.forEach((d) => {
+    d.tele_points = +d.tele_points;
+    d.jury_points = +d.jury_points;
+  });
+  data.sort((a, b) => a.total_points - b.total_points);
+
+  const target = '#piramid-container';
+  const options = { width: 500, height: 400, barHeight: 7.5, padding: 5 };
+  const country = "es";
+
+
+  pyramidBuilder(data, target, options, country);
+
+
+  d3.select(target)
+      .append("div")
+      .attr("class", "expand-icon")
+      .text("⤢")
+      .on("click", () => toggleFullscreen(target, data, options, country));
+
+  
+  document.addEventListener('keydown', function(e) {
+    if (e.key === 'Escape') {
+      const container = d3.select(target);
+      if (container.classed('fullscreen')) {
+        toggleFullscreen(target, data, options, country);
+      }
+    }
+  });
+});
+
